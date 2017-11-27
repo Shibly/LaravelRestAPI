83208b046e089282f65bfddd3d7a7edb7a72f3d0
Shibly <shibly.phy@gmail.com>
2017-11-27 11:44:30 +0600

    first commit

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+        
+
+    }
+}
