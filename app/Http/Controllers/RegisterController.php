ef07789af7adc8376b649b5211cfb13487161a92
Shibly <shibly.phy@gmail.com>
2017-11-27 13:17:09 +0600

    Added Laravel passport package

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\User;
 
 class RegisterController extends Controller
 {
     public function register(StoreUserRequest $request)
     {
-        
+
+
+        $user = new User();
+        $user->username = $request->username;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
+
+
+        //
 
     }
 }
